e624c141cdd76d80c4e432d04a41b8632f322211
Christoffer <nielsenchristoffer93@gmail.com>
2026-09-01 14:21:02 +0200

    Fix Zellij theme activation regex when theme line is commented in config.kdl

--- bin/omakub-sub/theme.sh
+++ bin/omakub-sub/theme.sh
@@ -6,7 +6,7 @@ THEME=$(gum choose "${THEME_NAMES[@]}" "<< Back" --header "Choose your theme" --
 if [ -n "$THEME" ] && [ "$THEME" != "<<-back" ]; then
   cp $OMAKUB_PATH/themes/$THEME/alacritty.toml ~/.config/alacritty/theme.toml
   cp $OMAKUB_PATH/themes/$THEME/zellij.kdl ~/.config/zellij/themes/$THEME.kdl
-  sed -i "s/theme \".*\"/theme \"$THEME\"/g" ~/.config/zellij/config.kdl
+  sed -i -E "s|^// *theme \".*\"|theme \"$THEME\"|g; s|^theme \".*\"|theme \"$THEME\"|g" ~/.config/zellij/config.kdl
   if [ -d "$HOME/.config/nvim" ]; then
     cp $OMAKUB_PATH/themes/$THEME/neovim.lua ~/.config/nvim/lua/plugins/theme.lua
   fi
