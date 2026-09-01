9aad7d911c3baf1e1e28724c35ac537cd59babdc
Christoffer <nielsenchristoffer93@gmail.com>
2026-09-01 13:42:12 +0200

    Fix wallpaper caching in set-gnome-theme.sh and update Cyberpunk/SynthWave wallpapers

--- themes/set-gnome-theme.sh
+++ themes/set-gnome-theme.sh
@@ -6,13 +6,19 @@ gsettings set org.gnome.desktop.interface gtk-theme "Yaru-$OMAKUB_THEME_COLOR-da
 gsettings set org.gnome.desktop.interface icon-theme "Yaru-$OMAKUB_THEME_COLOR"
 gsettings set org.gnome.desktop.interface accent-color "$OMAKUB_THEME_COLOR" 2>/dev/null || true
 
-BACKGROUND_ORG_PATH="$HOME/.local/share/omakub/themes/$OMAKUB_THEME_BACKGROUND"
+BACKGROUND_ORG_PATH="${OMAKUB_PATH:-$HOME/.local/share/omakub}/themes/$OMAKUB_THEME_BACKGROUND"
 BACKGROUND_DEST_DIR="$HOME/.local/share/backgrounds"
 BACKGROUND_DEST_PATH="$BACKGROUND_DEST_DIR/$(echo $OMAKUB_THEME_BACKGROUND | tr '/' '-')"
 
 if [ ! -d "$BACKGROUND_DEST_DIR" ]; then mkdir -p "$BACKGROUND_DEST_DIR"; fi
 
-[ ! -f $BACKGROUND_DEST_PATH ] && cp $BACKGROUND_ORG_PATH $BACKGROUND_DEST_PATH
-gsettings set org.gnome.desktop.background picture-uri "file://$BACKGROUND_DEST_PATH"
-gsettings set org.gnome.desktop.background picture-uri-dark "file://$BACKGROUND_DEST_PATH"
-gsettings set org.gnome.desktop.background picture-options 'zoom'
+if [ -f "$BACKGROUND_ORG_PATH" ]; then
+  # Always overwrite destination so updated wallpapers in the repository take effect immediately
+  cp -f "$BACKGROUND_ORG_PATH" "$BACKGROUND_DEST_PATH"
+  touch "$BACKGROUND_DEST_PATH"
+
+  # Force GNOME to re-read and apply the wallpaper
+  gsettings set org.gnome.desktop.background picture-uri "file://$BACKGROUND_DEST_PATH"
+  gsettings set org.gnome.desktop.background picture-uri-dark "file://$BACKGROUND_DEST_PATH"
+  gsettings set org.gnome.desktop.background picture-options 'zoom'
+fi
