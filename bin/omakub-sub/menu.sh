521cf83f0d98fe7c9ea1c91c9c66a5af05f23f5c
Christoffer <nielsenchristoffer93@gmail.com>
2026-09-01 14:23:07 +0200

    Restore header rendering in Omakub main menu and navigation

--- bin/omakub-sub/menu.sh
+++ bin/omakub-sub/menu.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+clear
+source $OMAKUB_PATH/bin/omakub-sub/header.sh
+
 if [ $# -eq 0 ]; then
-	SUB=$(gum choose "Theme" "Font" "Screensaver" "Shortcuts" "Update" "Install" "Uninstall" "Manual" "Quit" --height 12 --header "" | tr '[:upper:]' '[:lower:]')
+  SUB=$(gum choose "Theme" "Font" "Screensaver" "Shortcuts" "Update" "Install" "Uninstall" "Manual" "Quit" --height 12 --header "" | tr '[:upper:]' '[:lower:]')
 else
-	SUB=$1
+  SUB=$1
 fi
 
 [ -n "$SUB" ] && [ "$SUB" != "quit" ] && source $OMAKUB_PATH/bin/omakub-sub/$SUB.sh
